1c36c91d18c2848c07db7b89ad26489a6ac2311c
angular-cli <angular-cli@angular.io>
2016-12-13 15:38:58 +0700

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/angular2-firebase.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'angular2-firebase-app',
+  templateUrl: 'angular2-firebase.component.html',
+  styleUrls: ['angular2-firebase.component.css']
+})
+export class Angular2FirebaseAppComponent {
+  title = 'angular2-firebase works!';
+}
